38f98c5e5def1c004ccd9a0943f4b8e4282c1f34
Emna Zeghal <emna.zeghal@nioz.nl>
2021-11-15 14:56:42 +0100

    add_genus_species_tab

--- amplicon-seq-analysis.R
+++ amplicon-seq-analysis.R
@@ -35,7 +35,7 @@ library("heatmaply")
 
 
 #####data_import######
-tax <- as.matrix(read.delim('../../Data/runs/2021-10-29//asv/taxonomy_dada2/representative_seq_set_tax_assignments.txt', row.names = 1, na.strings = "NA"))
+tax <- as.matrix(read.delim('../../Data/runs/2021-10-29/asv/taxonomy_dada2/representative_seq_set_tax_assignments.txt', row.names = 1, na.strings = "NA"))
 tax <- tax_table(tax)
 otu <- as.matrix(read.delim("../../Data/runs/2021-10-29/asv/asv_table.txt", row.names = 1))
 otu <- otu_table(otu, taxa_are_rows = T)
@@ -43,8 +43,7 @@ map <- sample_data(read.delim("../../Data/RawData/mapping_file_details.txt", row
 physeq_object = merge_phyloseq(otu, tax, map)                 
 
 
-
-####basic_info#####
+####basic_info##############
 summarize_phyloseq(physeq_object)
 ntaxa(physeq_object)
 nsamples(physeq_object)  ###there's 71 samples in the OTU table and 74 in the mapping file any idea why? never mind, I found in the report that 3 of them had nothing whatsoever
@@ -85,7 +84,7 @@ taxo <- as.data.frame(physeq_object@tax_table)
 for (i in 1:nrow(taxo)) {
   for (y in 1:ncol(taxo)) {
     if 
-    (any(str_detect(taxo[i,y], c("uncultured","Uncultured","metagenome", "Metagenome","unknown", "Unknown","NA")))) {
+    (any(str_detect(taxo[i,y] , c("uncultured","Uncultured","metagenome","Metagenome","unknown","Unknown","NA")))) {
       taxo[i,y] <- "unassigned" }
   }
 }
@@ -102,16 +101,16 @@ physeq_object <- merge_phyloseq(physeq_object@otu_table, taxo, map) # merge upda
 ############### alpha div ###################
 summarize_phyloseq(physeq_object)
 alpha_tab <-microbiome::alpha(physeq_object, index = "all")
-write.csv(alpha_tab, file = "../../Analysis/alpha_div_indexes_microbiome_package.csv")
+#write.csv(alpha_tab, file = "../../Analysis/alpha_div_indexes_microbiome_package.csv")
 metad <- data.frame(physeq_object@sam_data) 
 metad$Shannon <- alpha_tab$diversity_shannon 
 metad$evenness_simpson <- alpha_tab$evenness_simpson 
-m <- subset_samples(physeq_object, timepoint %in% c("T1", "T6"))
+#m <- subset_samples(physeq_object, timepoint %in% c("T1", "T6"))
 
-p <- ggboxplot(metad, x = "Material", y = "evenness_simpson",
-               color = "Material", palette =c("#5FB233FF" ,"#6A7F93FF" ,"#F57206FF" ,"#EB0F13FF", "#8F2F8BFF", "#1396DBFF"),
-               add = "jitter", shape = "treatment", size = 1) + facet_wrap(~timepoint)
-p
+#p <- ggboxplot(metad, x = "Material", y = "evenness_simpson",
+#               color = "Material", palette =c("#5FB233FF" ,"#6A7F93FF" ,"#F57206FF" ,"#EB0F13FF", "#8F2F8BFF", "#1396DBFF"),
+#              add = "jitter", shape = "treatment", size = 1) + facet_wrap(~timepoint)
+#p
 
 
 a <- phyloseq::estimate_richness(physeq_object)
@@ -238,6 +237,8 @@ t2 <- tidy_physeq  %>% group_by(Sample) %>% mutate(Sample_rel_abund = Abundance 
 
 #write_csv(t2, "../../Analysis/tidy_data_NIOZ140.csv")  
 
+t2 <- read_csv("../../Analysis/tidy_data_NIOZ140.csv")
+
 
 ##############tables for each rank#############
 
@@ -330,7 +331,7 @@ ggplot(Genus, aes(x=Material, y= Genus_rep_rel_abund, fill=Genus))+
   geom_bar(stat="identity", position="stack")+ scale_color_brewer()+ 
   theme_classic()+  facet_grid (timepoint~treatment)
 
-Species <- t2 %>% select(treatment, timepoint, Material, description, Species, Species_rep_rel_abund, st_dev_Species_abund)%>% 
+Species <- t2 %>% select(treatment, timepoint, Material, description, Genus, Species, Species_rep_rel_abund, st_dev_Species_abund)%>% 
   distinct()
 
 head(Species)
@@ -413,7 +414,7 @@ transposed_otu <-  t(as(otu_table(physeq_object), "matrix"))
 #bray_distance<-vegdist(as(otu_table(physeq_object), "matrix"), method="bray")
 bray_distance2<-vegdist(transposed_otu, method="bray")
 
-#devtools::install_github("kylebittinger/usedist")
+#################devtools::install_github("kylebittinger/usedist")################
 
 sample_bray <- (as(bray_distance2, "matrix"))
 row.names(sample_bray) <- map2$description
@@ -804,113 +805,6 @@ microbiome::plot_taxa_prevalence(physeq_object, "Phylum")+ theme(legend.position
 
 
 ##################tidy_138#################
-
-tidy_psmelt <- function(physeq) {
-  ### INSERT Initial variable and rank name checking and modding from `psmelt`
-  # Get the OTU table with taxa as rows
-  rankNames = rank_names(physeq, FALSE)
-  sampleVars = sample_variables(physeq, FALSE) 
-  otutab <- otu_table(physeq)
-  if (!taxa_are_rows(otutab)) {
-    otutab <- t(otutab)
-  }
-  # Convert the otu table to a tibble in tidy form
-  tb <- otutab %>% 
-    as("matrix") %>%
-    tibble::as_tibble(rownames = "OTU") %>%
-    tidyr::gather("Sample", "Abundance", -OTU)
-  # Add the sample data if it exists
-  if (!is.null(sampleVars)) {
-    sam <- sample_data(physeq) %>%
-      as("data.frame") %>% 
-      tibble::as_tibble(rownames = "Sample")
-    tb <- tb %>%
-      dplyr::left_join(sam, by = "Sample")
-  }
-  # Add the tax table if it exists
-  if (!is.null(rankNames)) {
-    tax <- tax_table(physeq) %>%
-      as("matrix") %>%
-      tibble::as_tibble(rownames = "OTU")
-    tb <- tb %>%
-      dplyr::left_join(tax, by = "OTU")
-  }
-  tb %>%
-    arrange(desc(Abundance))
-  # Optional conversion to a data frame doesn't affect the speed/memory usage
-  # %>% as.data.frame
-}
-
-
-tidy_physeq <- tidy_psmelt(physeq_object)
-tidy_physeq$LinkerPrimerSequence <- NULL
-tidy_physeq$ReversePrimerSequence <- NULL
-tidy_physeq$InputFileName <- NULL
-tidy_physeq$BarcodeSequence <- NULL
-tidy_physeq$BarcodeSequence_1 <- NULL
-
-
-
-t2 <- tidy_physeq  %>% group_by(Sample) %>% mutate(Sample_rel_abund = Abundance / sum(Abundance)) %>% #relative abundance of each otu per sample
-  ungroup() %>%
-  group_by(description) %>% mutate( rep_rel_abund = Sample_rel_abund / sum(Sample_rel_abund)) %>% #relative abundance of each otu per number of samples in replicates
-  ungroup() %>% 
-  #Domain_section
-  group_by(Sample, Domain) %>% 
-  mutate(Domain_rel_abund_Sample = sum(Sample_rel_abund)) %>%  #domain relative abundance per sample 
-  ungroup() %>% 
-  group_by(description, Domain) %>% 
-  mutate(Domain_st_dev_abund_samples = sd(Domain_rel_abund_Sample)) %>% # standard dev of domain relative abundances between replicates of description (ployner_timepoint_treatment)
-  mutate(Domain_rep_rel_abund = sum(rep_rel_abund)) %>% #domain relative abundance per samples of desc 
-  ungroup() %>%
-  #Phylum_section
-  group_by(Sample, Phylum) %>% 
-  mutate(Phylum_rel_abund_Sample = sum(Sample_rel_abund)) %>%
-  ungroup() %>% 
-  group_by(description, Phylum) %>% 
-  mutate(st_dev_Phylum_abund = sd(Phylum_rel_abund_Sample)) %>%
-  mutate(Phyla_rep_rel_abund = sum(rep_rel_abund)) %>%
-  ungroup() %>% 
-  #Class_section
-  group_by(Sample, Class) %>% 
-  mutate(Class_rel_abund_Sample = sum(Sample_rel_abund)) %>%
-  ungroup() %>% 
-  group_by(description, Class) %>% 
-  mutate(st_dev_Class_abund = sd(Class_rel_abund_Sample)) %>%
-  mutate(Class_rep_rel_abund = sum(rep_rel_abund)) %>%
-  ungroup() %>% 
-  #Order_section
-  group_by(Sample, Order) %>% 
-  mutate(Order_rel_abund_Sample = sum(Sample_rel_abund)) %>%
-  ungroup() %>% 
-  group_by(description, Order) %>% 
-  mutate(st_dev_Order_abund = sd(Order_rel_abund_Sample)) %>%
-  mutate(Order_rep_rel_abund = sum(rep_rel_abund)) %>%
-  ungroup() %>% 
-  #Family_section
-  group_by(Sample, Family) %>% 
-  mutate(Family_rel_abund_Sample = sum(Sample_rel_abund)) %>%
-  ungroup() %>% 
-  group_by(description, Family) %>% 
-  mutate(st_dev_Family_abund = sd(Family_rel_abund_Sample)) %>%
-  mutate(Family_rep_rel_abund = sum(rep_rel_abund)) %>%
-  ungroup() %>% 
-  #Genus_section
-  group_by(Sample, Genus) %>% 
-  mutate(Genus_rel_abund_Sample = sum(Sample_rel_abund)) %>%
-  ungroup() %>% 
-  group_by(description, Genus) %>% 
-  mutate(st_dev_Genus_abund = sd(Genus_rel_abund_Sample)) %>%
-  mutate(Genus_rep_rel_abund = sum(rep_rel_abund)) %>%
-  ungroup() %>% 
-  #Species_section
-  group_by(Sample, Species) %>% 
-  mutate(Species_rel_abund_Sample = sum(Sample_rel_abund)) %>%
-  ungroup() %>% 
-  group_by(description, Species) %>% 
-  mutate(st_dev_Species_abund = sd(Species_rel_abund_Sample)) %>%
-  mutate(Species_rep_rel_abund = sum(rep_rel_abund)) %>%
-  ungroup()
 
 
 ##############tables_138#############
